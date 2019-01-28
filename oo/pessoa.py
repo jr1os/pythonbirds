b589de4ad9ca2d968a0668dd0f01bbba654f28b9
j1os <m1n1stry@hotmail.com>
2019-01-28 11:20:11 -0200

    Criado atribudo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,9 @@
 class Pessoa:
-    def __init__(self, nome = None, idade=46):
+    def __init__(self, *filhos, nome = None, idade=46):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
+
 
 
     def cumprimentar(self):
@@ -9,10 +11,12 @@ class Pessoa:
 
 
 if __name__ == '__main__':
-    p = Pessoa('Luciano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    p.nome = 'Jesus'
-    print(p.nome)
-    print(p.idade)
+    jesus = Pessoa(nome='Jesus')
+    luciano = Pessoa(jesus, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    print(luciano.idade)
+    for filho in luciano.filhos:
+        print(filho.nome)
